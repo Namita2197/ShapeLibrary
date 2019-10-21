04f9ae3e4b0f28f465234e3bfdc19d249842815e
Namita Raghuvanshi <raghuvanshinamita@gmail.com>
2019-10-21 15:06:25 -0600

    Rectangle

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -8,7 +8,11 @@ public class Rectangle {
         corner1 = new Point(x1, y1);
         corner2 = new Point(x2, y2);
         corner3 = new Point(x3, y3);
-        corner3 = new Point(x4, y4);
+        corner4 = new Point(x4, y4);
+        if (corner1 == null || corner2 == null || corner3 == null || corner4 == null)
+            throw new ShapeException("Invalid vertex or vertices");
+
+        checkRectangleValidity(corner1, corner2, corner3, corner4);
     }
 
     public Rectangle(Point corner1, Point corner2, Point corner3, Point corner4) throws ShapeException {
@@ -19,20 +23,59 @@ public class Rectangle {
         if (corner1 == null || corner2 == null || corner3 == null || corner4 == null)
             throw new ShapeException("Invalid vertex or vertices");
 
-        this.computeLengthBreadth(); // assert rectangle properties to make sure this is a valid rectangle
-
+        checkRectangleValidity(corner1, corner2, corner3, corner4);
     }
 
-    public Point getCorner1() throws ShapeException { return corner1.copy(); }
-    public Point getCorner2() throws ShapeException { return corner2.copy(); }
-    public Point getCorner3() throws ShapeException { return corner3.copy(); }
-    public Point getCorner4() throws ShapeException { return corner4.copy(); }
+    public Point getCorner1() throws ShapeException { return corner1; }
+    public Point getCorner2() throws ShapeException { return corner2; }
+    public Point getCorner3() throws ShapeException { return corner3; }
+    public Point getCorner4() throws ShapeException { return corner4; }
 
     public void move(double deltaX, double deltaY) throws ShapeException {
         corner1.move(deltaX, deltaY);
         corner2.move(deltaX, deltaY);
         corner3.move(deltaX, deltaY);
         corner4.move(deltaX, deltaY);
+    }
+
+    public void scale(double scaleFactor) throws ShapeException {
+        Validator.validatePositiveDouble(scaleFactor, "Invalid scale factor");
+        double y1=corner1.getY();
+        double x2=corner2.getX();
+        double y2=corner2.getY();
+        double x4=corner4.getX();
+        y1=(y1*scaleFactor)-y1;
+        x2=(x2*scaleFactor)-x2;
+        y2=(y2*scaleFactor)-y2;
+        x4=(x4*scaleFactor)-x4;
+
+        corner1.move(0,y1);
+        corner2.move(x2,y2);
+        corner4.move(x4,0);
+    }
+
+    public double computeArea() throws ShapeException {
+        Line length=new Line(corner1, corner2);
+        Line breadth = new Line(corner2, corner4);
+        double lengthMeasure=length.computeLength();
+        double breadthMeasure=breadth.computeLength();
+        return lengthMeasure*breadthMeasure;
+    }
+    public void checkRectangleValidity(Point corner1,Point corner2,Point corner3, Point corner4) throws ShapeException {
+        Line line1 = new Line(corner1, corner2);
+        Line line2 = new Line(corner2, corner4);
+        Line line3 = new Line(corner1, corner3);
+        Line line4 = new Line(corner3, corner4);
+        Line hypotenuse1 = new Line(corner1, corner4);
+
+        double lengthLine1 = line1.computeLength();
+        double lengthLine2 = line2.computeLength();
+        double lengthLine3 = line3.computeLength();
+        double lengthLine4 = line4.computeLength();
+        double lengthHypotenuse = hypotenuse1.computeLength();
+
+        assert Math.round(Math.sqrt(Math.pow(lengthLine1, 2) + Math.pow(lengthLine2, 2))) == Math.round(lengthHypotenuse);
+        assert Math.round(Math.sqrt(Math.pow(lengthLine3, 2) + Math.pow(lengthLine4, 2))) == Math.round(lengthHypotenuse);
     }
 
 }
---
+++ src/test/java/RectangleTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RectangleTest {
+
+    @Test
+    public void getCorner1() {
+    }
+
+    @Test
+    public void getCorner2() {
+    }
+
+    @Test
+    public void getCorner3() {
+    }
+
+    @Test
+    public void getCorner4() {
+    }
+
+    @Test
+    public void move() {
+    }
+
+    @Test
+    public void scale() {
+    }
+
+    @Test
+    public void computeArea() {
+    }
+
+    @Test
+    public void checkRectangleValidity() {
+    }
+}

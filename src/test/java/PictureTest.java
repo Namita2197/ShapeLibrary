53383fb7231f33df86753b5198b17a60541d5cfe
Namita Raghuvanshi <raghuvanshinamita@gmail.com>
2019-10-22 00:39:25 -0600

    picture testing done

--- src/test/java/PictureTest.java
+++ src/test/java/PictureTest.java
@@ -1,5 +1,21 @@
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 import static org.junit.Assert.*;
 
 public class PictureTest {
+    @Test
+    public void render() throws Exception {
+
+        Picture picture = new Picture("picture.jpg");
+        BufferedImage bufferedImage = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D graphics = bufferedImage.createGraphics();
+        graphics.setColor(Color.white);
+
+        picture.render(graphics, 0, 0, 100, 200);
+    }
 
 }
